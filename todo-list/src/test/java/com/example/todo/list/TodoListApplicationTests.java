22fcfc7c6b9c61f060a4c40fa2134f4173abbbed
fnascimento <felipe.basina@gmail.com>
2020-03-05 22:17:24 -0300

    Adicionado dependencia para testes de unidade + configuracao para travis-ci.

--- todo-list/src/test/java/com/example/todo/list/TodoListApplicationTests.java
+++ todo-list/src/test/java/com/example/todo/list/TodoListApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.todo.list;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodoListApplicationTests {
+public class TodoListApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
